eed585664827a02a76d527176e2e6b0ad246f722
Im-Vishwjeet <vishwjeetmeena2811@gmail.com>
2025-04-13 12:29:04 +0530

    Project SetUp

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {TokenLaunchpad} from "../src/TokenLaunchpad.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract TokenLaunchpadScript is Script {
+    TokenLaunchpad public tokenLaunchpad;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        tokenLaunchpad = new TokenLaunchpad();
 
         vm.stopBroadcast();
     }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {TokenLaunchpad} from "../src/TokenLaunchpad.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract TokenLaunchpadTest is Test {
+    TokenLaunchpad public tokenLaunchpad;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        tokenLaunchpad = new TokenLaunchpad();
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_TokenLaunchpad() public {
+        
     }
 }
